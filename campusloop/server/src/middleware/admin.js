80b3b8faef4c6c2b66283bb83bc6fbfa66f31bca
Ismail <ismail.elkoubyadi001@umb.edu>
2026-03-14 04:16:00 -0400

    Delete campusloop directory

--- campusloop/server/src/middleware/admin.js
+++
@@ -1,6 +0,0 @@
-export function adminRequired(req, res, next) {
-  if (!req.user || req.user.role !== "ADMIN") {
-    return res.status(403).json({ error: "Admin access required" });
-  }
-  next();
-}
